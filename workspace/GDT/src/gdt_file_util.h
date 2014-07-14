51215cd2496150bbcd772603a74b4b3c08228475
PKUKitty <pkukitty@gmail.com>
2014-07-14 16:45:55 +0800

    Update gdt_file_util.h
    Add the File Util class

--- workspace/GDT/src/gdt_file_util.h
+++ workspace/GDT/src/gdt_file_util.h
@@ -1,2 +1,25 @@
-#include "gdt_sys.h"
+/*
+ * gdt_file_util.h
+ *
+ *  Created on: May 5, 2014
+ *      Author: autoyujun
+ */
 
+#ifndef GDT_FILE_UTIL_H_
+#define GDT_FILE_UTIL_H_
+
+#include "gdt_sys.h"
+using namespace std;
+
+/*
+  This class includes all functions during file operation
+
+*/
+class GDTFileUtil{
+  
+  
+  
+  
+}
+
+#endif /* GDT_FILE_UTIL_H_*/
