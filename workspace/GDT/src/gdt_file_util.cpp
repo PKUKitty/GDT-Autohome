426c50715fbfc7b1696f8ebb77602c38148246b4
PKUKitty <pkukitty@gmail.com>
2014-07-14 17:29:22 +0800

    Update gdt_file_util.cpp

--- workspace/GDT/src/gdt_file_util.cpp
+++ workspace/GDT/src/gdt_file_util.cpp
@@ -1,1 +1,57 @@
+ /*
+  
+ 
+ */
+ 
 #include "gdt_file_util.h"
+
+namespace gdt{
+  
+  int open(const char* name, int flags, mode_t mode){
+    
+    
+    
+  }
+  
+  int close(int fd){
+    
+    
+  }
+  
+  int dup(int fd){
+    
+    
+  }
+  
+  int fsync(int fd){
+    
+  }
+  
+  int truncate(int fd){
+    
+  }
+  
+  ssize_t read(int fd, void* buf, size_t n){
+    
+  }
+  
+  ssize_t pread(int fd, void* buf, size_t n, off_t offset){
+    
+  }
+  
+  ssize_t write(int fd, void* buf, size_t n){
+    
+  }
+  
+  ssize_t pwrite(int fd, void* buf, size_t n, off_t offset){
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  }
