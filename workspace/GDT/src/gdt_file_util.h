7166352e16ca588a6b84a84099e328fbf087744d
PKUKitty <pkukitty@gmail.com>
2014-07-14 17:13:28 +0800

    Update gdt_file_util.h
    Add some functions for file util, including open,close,read,write.

--- workspace/GDT/src/gdt_file_util.h
+++ workspace/GDT/src/gdt_file_util.h
@@ -15,11 +15,22 @@ using namespace std;
   This class includes all functions during file operation
 
 */
-class GDTFileUtil{
+namespace gdt{
+      
+      int open(const char* name, int flags, mode_t mode = 0666);
+      int close(int fd);
+      int dup(int fd);
+      int fsync(int fd);
+      int truncate(const char* path, off_t len);
+      
+      ssize_t read(int fd, void* buf, size_t n);
+      ssize_t pread(int fd, void* buf, size_t n, off_t offset);
+      
+      ssize_t write(int fd, void* buf, size_t n);
+      ssize_t pwrite(int fd, void* buf, size_t n, off_t offset);
   
   
-  
-  
-}
+  }
+
 
 #endif /* GDT_FILE_UTIL_H_*/
